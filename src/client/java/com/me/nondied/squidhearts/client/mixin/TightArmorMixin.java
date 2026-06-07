1bebab34edb997548aa350f858c9e353d86ae232
nondieddd <nondieddd@gmail.com>
2026-06-06 20:38:16 -0600

    armadura

---
+++ src/client/java/com/me/nondied/squidhearts/client/mixin/TightArmorMixin.java
@@ -0,0 +1,31 @@
+package com.me.nondied.squidhearts.client.mixin;
+
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.feature.ArmorFeatureRenderer;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterials;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ArmorFeatureRenderer.class)
+public abstract class TightArmorMixin<T extends LivingEntity, M extends BipedEntityModel<T>, A extends BipedEntityModel<T>> {
+
+    @Inject(
+            method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/LivingEntity;FFFFFF)V",
+            at = @At("HEAD")
+    )
+    private void makeArmorTight(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, T entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch, CallbackInfo ci) {
+        ItemStack chestStack = entity.getEquippedStack(EquipmentSlot.CHEST);
+
+        if (chestStack.getItem() instanceof ArmorItem armorItem && armorItem.getMaterial() == ArmorMaterials.LEATHER) {
+            matrices.scale(0.95f, 1.0f, 0.95f);
+        }
+    }
+}
